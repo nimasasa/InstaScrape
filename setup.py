51c23991140c6dfcf0905e139abeaaee326a98fa
nimasasa <nimasasa@yahoo.co.uk>
2019-09-24 11:17:27 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="InstaScrape",
+    version="1.0.0",
+    author="Nima",
+    description="Read insta to soup",
+    url="https://github.com/nimasasa/InstaScrape/",
+    license="Free",
+    packages=['InstaScrape'],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GAP Inc. License",
+        "Operating System :: OS Independent",
+    ],
+)
